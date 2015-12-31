c1ac8c79e6961d8d21df9e513e2fec8a00dc595a
Dianna <dhummel1210@gmail.com>
2015-12-31 13:26:25 -0500

    All code put into functions

--- myToDoList2.py
+++ myToDoList2.py
@@ -24,27 +24,30 @@ def add_to_list():
     todo_list.append(new_item)
     print("Added {}.  List now has {} items.".format(new_item, len(todo_list)))
 
-while True:
-    #ask for new items to be added
-    new_item = input("~ ")
-    
-    #be able to quit app
-    if new_item == "DONE":
-        show_list()
-        break
-    
-    #have a HELP cmd
-    elif new_item == "HELP":
-        show_help()
-        continue
-    
-    #have a SHOW cmd
-    elif new_item == "SHOW":
-        show_list()
-        print("Continue adding to your list.")
-        continue
+def main():
+    while True:
+        #ask for new items to be added
+        new_item = input("~ ")
+        
+        #be able to quit app
+        if new_item == "DONE":
+            show_list()
+            break
+        
+        #have a HELP cmd
+        elif new_item == "HELP":
+            show_help()
+            continue
+        
+        #have a SHOW cmd
+        elif new_item == "SHOW":
+            show_list()
+            print("Continue adding to your list.")
+            continue
 
-    add_to_list()
+        add_to_list()
+
+main()        
      
       
 
